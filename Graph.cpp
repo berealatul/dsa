969f6bc758c014ec38d9ead9ce68e80a785c7b75
Atul Prakash <berealatul@gmail.com>
2025-01-30 23:28:27 +0530

    Revert "Extracted validPath into seperate file"
    This reverts commit 6361f7a9ab26d41ac76ee0e1db4f74ed369b17fc.

--- Graph.cpp
+++ Graph.cpp
@@ -41,3 +41,64 @@ void Graph::bfs(const vector<vector<int>> &adjList, vector<bool> &visited, const
         }
     }
 }
+
+bool Graph::validPath(int n, vector<vector<int>> &edges, int source, int destination)
+{
+    // converting edge list of a graph to adjacency list
+    vector<vector<int>> adjList(n);
+    for (auto edge : edges)
+    {
+        adjList[edge[0]].push_back(edge[1]);
+        adjList[edge[1]].push_back(edge[0]);
+    }
+
+    vector<bool> visited(n, false);
+    // return dfsMode(adjList, visited, source, destination);
+    return bfsMode(adjList, visited, source, destination);
+}
+
+bool dfsMode(const vector<vector<int>> &adjList, vector<bool> &visited, int source, int destination)
+{
+    if (source == destination)
+        return true;
+    visited[source] = true;
+
+    for (auto neighbour : adjList[source])
+    {
+        if (!visited[neighbour] && dfsMode(adjList, visited, neighbour, destination))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool bfsMode(vector<vector<int>> &adjList, vector<bool> &visited, int source, int destination)
+{
+    if (source == destination)
+        return true;
+
+    queue<int> q;
+    q.push(source);
+    visited[source] = true;
+
+    while (!q.empty())
+    {
+        int curr = q.front();
+        q.pop();
+
+        for (auto neighbour : adjList[curr])
+        {
+            if (neighbour == destination)
+                return true;
+
+            else if (!visited[neighbour])
+            {
+                visited[neighbour] = true;
+                q.push(neighbour);
+            }
+        }
+    }
+
+    return false;
+}
