a86e4b907e4991c46c6db07821826fb16c3e4544
Atul Prakash <berealatul@gmail.com>
2025-01-30 21:03:31 +0530

    Implemented DFS Function

--- Graph.cpp
+++ Graph.cpp
@@ -3,3 +3,17 @@
 using namespace std;
 
 #include "Graph.h"
+
+void Graph::dfs(const vector<vector<int>> &adjList, vector<bool> &visited, const int start)
+{
+    visited[start] = true;
+    cout << start << " ";
+
+    for (const auto neighbour : adjList[start])
+    {
+        if (!visited[neighbour])
+        {
+            dfs(adjList, visited, neighbour);
+        }
+    }
+}
