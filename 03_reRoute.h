cae666e05a4b2419c8da4ce1f90d19a3134cd6b9
Atul Prakash <berealatul@gmail.com>
2025-01-31 00:07:05 +0530

    Initial Prototype

---
+++ 03_reRoute.h
@@ -0,0 +1,15 @@
+#ifndef REROUTE
+#define REROUTE
+
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    int minReorder(int n, vector<vector<int>> &connections)
+    {
+    }
+};
+
+#endif
