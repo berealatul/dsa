8f01eee7c94e39fd0a6dafb8c4029ded910d0f96
Atul Prakash <berealatul@gmail.com>
2025-01-30 21:22:23 +0530

    added leetcode question 'Find if Path Exists in Graph'

--- Graph.h
+++ Graph.h
@@ -12,6 +12,10 @@ public:
 
     // traverse given graph in form of adjacency list through bfs
     static void bfs(const vector<vector<int>> &adjList, vector<bool> &visited, const int start);
+
+    // https://leetcode.com/problems/find-if-path-exists-in-graph
+    // find if path from source to destination exist in an undirected graph. it can be disconnected
+    static bool validPath(int n, vector<vector<int>> &edges, int source, int destination);
 };
 
 #endif
